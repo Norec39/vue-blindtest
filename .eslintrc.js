490dc17e0e79df6ba5f151baa99b12e9bf43937f
Louis Garret <louis@garret.ovh>
2020-06-19 10:32:02 +0200

    :package: Enable ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@vue/airbnb'
-  ],
-  rules: {
-    "indent": [
-      "error",
-      "tab"
-    ],
-    "no-tabs": "off",
-    "no-console": "off",
-    "no-mixed-spaces-and-tabs": "off"
-  }
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'plugin:nuxt/recommended'
+	],
+	rules: {
+		indent: [
+			'error',
+			'tab'
+		],
+		'no-tabs': 'off',
+		'no-console': 'off',
+		'no-mixed-spaces-and-tabs': 'off'
+	}
 }
