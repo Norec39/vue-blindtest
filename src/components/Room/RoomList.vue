fd129ed6dafefbdec185140c91202fcad0b14047
francois.de-vathaire@epitech.eu <francois.de-vathaire@epitech.eu>
2020-07-30 17:25:16 +0200

    addofnewroomlistview

--- src/components/Room/RoomList.vue
+++ src/components/Room/RoomList.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="container mt-3">
+		<button @click="fetchRooms">
+			<i class="fas fa-sync-alt"></i>
+		</button>
 		<ul v-if="rooms.length">
 			<li v-for="r in rooms" :key="r['@id']">
 				{{ r.name }}
@@ -11,24 +14,29 @@
 </template>
 
 <script>
+import to from '../../utils/to';
+import notify from '../../utils/notify';
+
 export default {
 	name: 'RoomList',
 	data: () => ({
 		rooms: [],
 	}),
 	async created() {
-		this.fetchRooms();
+		await this.fetchRooms();
 	},
 	methods: {
-		fetchRooms() {
-			const es = new EventSource(
-				'http://localhost:3000/.well-known/mercure?topic=http://localhost:8001/api/games/{id}',
-			);
-			// eslint-disable-next-line no-shadow
-			es.onmessage = ({ data }) => {
-				const rooms = JSON.parse(data);
-				this.rooms.push(rooms);
-			};
+		async fetchRooms() {
+			console.log('fetch');
+			const [err, response] = await to(this.axios.get(
+				`${this.$serverApiLink}/rooms/`,
+			));
+
+			if (err) {
+				return notify('Error !', 'Can\'t find rooms', 'error');
+			}
+			this.rooms = response.data;
+			return true;
 		},
 	},
 };
